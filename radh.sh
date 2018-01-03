a981ca3b7ed1640aa5936bde0c6fbe92acff98d8
Radhakrishna Hari <radh@microsoft.com>
2018-01-03 14:50:13 -0800

    add .net 2.0 install to the package

--- radh.sh
+++ radh.sh
@@ -10,5 +10,12 @@ sudo ufw allow 5432/tcp
 sudo service ufw restart
 sudo ufw status
 
-sudo apt-get install mariadb-server
 
+#
+# Install .net core
+#
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-zesty-prod zesty main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get update
+sudo apt-get --yes --force-yes install dotnet-sdk-2.0.0
